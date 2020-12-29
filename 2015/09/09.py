6f7dcf3965b040e109d2a50d8e48a0a530103356
Eric Alexandersson <eric.alexandersson@ericsson.com>
2020-12-29 12:55:32 +0100

    2015

---
+++ 2015/09/09.py
@@ -0,0 +1,81 @@
+import re
+from itertools import permutations
+
+
+def read_input_file(path):
+    with open(path) as f:
+        content = f.read()
+        return [c for c in content.split("\n")]
+
+
+def parse_data(data):
+    route = []
+    locations = []
+    for d in data:
+        src, target, dist = re.findall(r"(.*) to (.*) = (\d+)", d).pop()
+        route.append((src, target, int(dist)))
+        locations.append(src)
+        locations.append(target)
+    return route, list(set(locations))
+
+
+def find_distance(routes: list, loc1: str, loc2: str):
+    for route in routes:
+        if (loc1 in route[0] or loc1 in route[1]) and (loc2 in route[0] or loc2 in route[1]):
+            return route[2]
+
+
+def part_1(data):
+    route_data, locations = parse_data(data)
+    all_routes = permutations(locations)
+    shortest = None
+    for routes in all_routes:
+        total_distance = 0
+        for idx, r in enumerate(routes):
+            if idx == 0:
+                continue
+            loc1 = routes[idx-1]
+            loc2 = routes[idx]
+            total_distance += find_distance(route_data, loc1, loc2)
+        if not shortest:
+            print(f"First ({routes}): {total_distance}")
+            shortest = total_distance
+        elif total_distance < shortest:
+            print(f"new shortest! ({routes}): {total_distance}")
+            shortest = total_distance
+    return shortest
+
+
+def part_2(data):
+    route_data, locations = parse_data(data)
+    all_routes = permutations(locations)
+    longest = None
+    for routes in all_routes:
+        total_distance = 0
+        for idx, r in enumerate(routes):
+            if idx == 0:
+                continue
+            loc1 = routes[idx - 1]
+            loc2 = routes[idx]
+            total_distance += find_distance(route_data, loc1, loc2)
+        if not longest:
+            print(f"First ({routes}): {total_distance}")
+            longest = total_distance
+        elif total_distance > longest:
+            print(f"new longest! ({routes}): {total_distance}")
+            longest = total_distance
+    return longest
+
+
+if __name__ == '__main__':
+    print("----- part 1 -----")
+    filepath = "09_input.txt"
+    data = read_input_file(filepath)
+    ans = part_1(data)
+    print(f"answer: {ans}")
+
+    print("----- part 2 -----")
+    filepath = "09_input.txt"
+    data = read_input_file(filepath)
+    ans = part_2(data)
+    print(f"answer: {ans}")
