a2eb319f4a3fe6cd3bbfcf528cbf90e6314abb45
Eric Alexandersson <eric.alexandersson@ericsson.com>
2022-12-01 07:04:19 +0100

    2022-day1

---
+++ 2022/01/01.py
@@ -0,0 +1,45 @@
+def read_input_file(file_name: str):
+    with open(file_name) as f:
+        content = f.read()
+        elves_food = content.split("\n\n")
+        result = []
+        for cal in elves_food:
+            cals = cal.split("\n")
+            elf_calories = [int(c) for c in cals]
+            result.append(elf_calories)
+        return result
+
+
+def part_1():
+    dd = read_input_file("input.txt")
+    highest_cal = 0
+
+    for x, elf in enumerate(dd):
+        sum_cal = sum(elf)
+        if sum_cal >= highest_cal:
+            highest_cal = sum_cal
+
+    return highest_cal
+
+
+def part_2():
+    dd = read_input_file("input.txt")
+    highest_cal = [0, 0, 0]
+
+    for x, elf in enumerate(dd):
+        sum_cal = sum(elf)
+        minpos = highest_cal.index(min(highest_cal))
+        if sum_cal > highest_cal[minpos]:
+            highest_cal[minpos] = sum_cal
+
+    return sum(highest_cal)
+
+
+if __name__ == '__main__':
+    print("----- part 1 -----")
+    part1_ans = part_1()
+    print(f"answer: {part1_ans}")
+
+    print("----- part 2 -----")
+    part2_ans = part_2()
+    print(f"answer: {part2_ans}")
